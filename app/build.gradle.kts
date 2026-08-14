dfea51332c2856496f688aa65eac95fe2f1eb2c0
FabioVF26 <fabio.fiordi@vigilfuoco.it>
2026-08-14 16:28:40 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,11 +26,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    packaging {
+        packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
